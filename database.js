4ef65b4709b19757b0763b0c94a03bdb7e35ad5f
RudranshGautam <158027721+RudranshGautam@users.noreply.github.com>
2024-09-07 16:44:45 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    logging: false,  // Disable logging
+    host: process.env.DB_HOST
+});
+
+module.exports = sequelize;
